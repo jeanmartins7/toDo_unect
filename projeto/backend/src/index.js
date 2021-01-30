fc0b4b50b32c418c10f0a03f652f25ce66a3cb9f
Jean Martins <jeanmarcel8@gmail.com>
2021-01-30 02:44:36 -0300

    Update index.js

--- projeto/backend/src/index.js
+++ projeto/backend/src/index.js
@@ -3,10 +3,6 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/alltodo", express.static("public"));
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extends: false }));
 
